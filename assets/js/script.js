68a38f8e50a5cba1801918b67aa0c9f53ee63239
Sami El Sinnari <samisinnari@gmail.com>
2020-12-09 10:40:55 -0800

    style&script : Navbar layout created for mobile vp

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+/*    NAVBAR    */
+
+const navbarToggler = document.querySelector (".hamburger-menu");
+
+const navbar = document.querySelector(".navbar");
+
+navbarToggler.addEventListener("click", () => {
+  navbar.classList.toggle("close");
+});
