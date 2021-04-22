17a05415153f77d391519e001bd584753f8b5972
Sami El Sinnari <samisinnari@gmail.com>
2021-04-22 00:17:30 -0700

    js:  added maps location with marker

---
+++ assets/js/maps.js
@@ -0,0 +1,13 @@
+function initMap() {
+    const newyork = { lat: 40.86751 , lng: -73.96054 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 9,
+      center: newyork,
+    });
+
+    const marker = new google.maps.Marker({
+      position: newyork,
+      map: map,
+    });
+  }
+  
