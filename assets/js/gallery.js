5b2cc2b95fc20f53cd6d1224d6b96d447e78913a
Sami El Sinnari <samisinnari@gmail.com>
2021-04-21 22:24:28 -0700

    js: selected with querySelector

---
+++ assets/js/gallery.js
@@ -0,0 +1,12 @@
+
+/*    GALLERY    */
+
+
+const gallery  = document.querySelectorAll(".image"),
+previewBox = document.querySelector(".preview-box"),
+previewImg = previewBox.querySelector("img"),
+closeIcon = previewBox.querySelector(".icon"),
+currentImg = previewBox.querySelector(".current-img"),
+totalImg = previewBox.querySelector(".total-img"),
+shadow = document.querySelector(".shadow");
+
