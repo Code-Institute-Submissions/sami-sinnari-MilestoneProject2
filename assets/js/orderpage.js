2e37fa520f50081fc77e4d013a5ff4f8faf29f89
Sami El Sinnari <samisinnari@gmail.com>
2021-04-27 18:21:14 -0700

    js: assigned variables for orderpage

---
+++ assets/js/orderpage.js
@@ -0,0 +1,26 @@
+let productDOM = document.querySelector(".product__center");
+
+let cartDOM = document.querySelector(".cart");
+
+let cartContent = document.querySelector(".cart__centent");
+
+let openCart = document.querySelector(".cart__icon");
+
+let closeCart = document.querySelector(".close__cart");
+
+let overlay = document.querySelector(".cart__overlay");
+
+let cartTotal = document.querySelector(".cart__total");
+
+let clearCartBtn = document.querySelector(".clear__cart");
+
+let itemTotals = document.querySelector(".item__total");
+
+let cart = [];
+
+let buttonDOM = [];
+
+
+
+
+
